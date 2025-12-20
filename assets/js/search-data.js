7b0cd3fc69eb748ac9e3715f483d26c900fcbde9
fenil-b <fenildbardoliya@gmail.com>
2025-12-20 22:49:43 +0000

    Deploying to gh-pages from @ fenil-b/fenil-b.github.io@0ea42e586fb29d521c9261952f89962255136def 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,17 +11,10 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-submenus",
-          title: "submenus",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/_pages/dropdown/";
+            window.location.href = "/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
